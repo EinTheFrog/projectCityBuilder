88ff2fa11fbfc1bbd8e273d1812e3b141d2687d6
Maxim <alpatskij.mi@edu.spbstu.ru>
2020-10-12 20:18:44 +0300

    classwork refactoring

--- src/main/java/render/Main.java
+++ src/main/java/render/Main.java
@@ -2,11 +2,8 @@ package render;
 
 
 import javafx.application.Application;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import stages.MainMenu;
-
-import java.io.IOException;
 
 /**
  * класс, запускающий приложение
